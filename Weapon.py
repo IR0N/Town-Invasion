85ff99ea817c31d5acdb73969f7a76f052bf9786
Joseph <josephahrens86@yahoo.com>
2016-03-14 23:21:22 -0500

    Started the Weapon class
    Started working on the Weapon class. Setting up the getWeapon() function
    to choose a random weapon. Setting up the variance() function to assign
    a random adjective to a weapon to eventually influence the weapon's
    damage range.

---
+++ Weapon.py
@@ -0,0 +1,15 @@
+from random import choice
+
+def variance():
+	meleeAdj = ["Broken", "Rusty", "Bent", "", "Small", "Large", "Pristine"]
+	rangedAdj = ["Broken", "Jammed", "Functional", "Rusty", "", "Small", "Large", "Pristine"]
+	#assign variance adjective to weapon to receive weapon damage potential
+	
+def getWeapon():
+	melee = ["Sword", "Baseball Bat", "Axe", "Polearm", "Chainsaw", "Spear", "Scythe", "Dagger", "Knife", "Club", "Hammer", "Stick", "Whip"]
+	ranged = ["Crossbow", "Slingshot", "Boomerang", "Javelin", "Shotgun", "Pistol"]
+	print(choice(melee))
+	print(choice(ranged))
+
+getWeapon()
+	
