f61b150f3bae8908e6a7795e3293c966d44c0e96
Jeremiah Megel <github@jeremiahm.com>
2016-03-16 21:22:53 -0500

    Rewrote Weapon module

--- Weapon.py
+++ Weapon.py
@@ -1,15 +1,46 @@
 from random import choice
 
-def variance():
-	meleeAdj = ["Broken", "Rusty", "Bent", "", "Small", "Large", "Pristine"]
-	rangedAdj = ["Broken", "Jammed", "Functional", "Rusty", "", "Small", "Large", "Pristine"]
-	#assign variance adjective to weapon to receive weapon damage potential
-	
-def getWeapon():
-	melee = ["Sword", "Baseball Bat", "Axe", "Polearm", "Chainsaw", "Spear", "Scythe", "Dagger", "Knife", "Club", "Hammer", "Stick", "Whip"]
-	ranged = ["Crossbow", "Slingshot", "Boomerang", "Javelin", "Shotgun", "Pistol"]
-	print(choice(melee))
-	print(choice(ranged))
+class Weapon:
+	def __init__(self):
+		self.variety = choice(self.varieties)
+		self.condition = choice(list(self.conditions))
 
-getWeapon()
-	
+	def getCondition(self):
+		return self.condition
+
+	def getVariety(self):
+		return self.variety
+
+	class Variety:
+		def __init__(self, name, pickupChance):
+			self.name = name
+			self.pickupChance = pickupChance
+
+		def getName(self):
+			return self.name
+
+		def getPickupChance(self):
+			return self.pickupChance
+
+	class Condition:
+		def __init__(self, name):
+			self.name = name
+
+		def getName(self):
+			return self.name
+
+class Weapon_Melee(Weapon):
+	varieties = [Weapon.Variety("sword", 0), Weapon.Variety("baseball bat", 0), Weapon.Variety("axe", 0),
+		Weapon.Variety("polearm", 0), Weapon.Variety("chainsaw", 0), Weapon.Variety("spear", 0),
+		Weapon.Variety("scythe", 0), Weapon.Variety("dagger", 0), Weapon.Variety("knife", 0),
+		Weapon.Variety("club", 0), Weapon.Variety("hammer", 0), Weapon.Variety("stick", 0),
+		Weapon.Variety("whip", 0)]
+	conditions = [Weapon.Condition("broken"), Weapon.Condition("rusty"), Weapon.Condition("bent"),
+		Weapon.Condition("small"), Weapon.Condition("large"), Weapon.Condition("pristine")]
+
+class Weapon_Ranged(Weapon):
+	varieties = [Weapon.Variety("crossbow", 0), Weapon.Variety("slingshot", 0), Weapon.Variety("boomerang", 0),
+		Weapon.Variety("javelin", 0), Weapon.Variety("shotgun", 0), Weapon.Variety("pistol", 0)]
+	conditions = [Weapon.Condition("broken"), Weapon.Condition("jammed"), Weapon.Condition("functional"),
+		Weapon.Condition("rusty"), Weapon.Condition("small"), Weapon.Condition("large"),
+		Weapon.Condition("pristine")]
